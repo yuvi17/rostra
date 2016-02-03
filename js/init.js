6a00b061dd1effef98730a8bd864488e34f6f11d
Yuvraj Jaiswal <Yuvraj Jaiswal>
2016-02-04 00:28:14 +0530

    Added files

---
+++ js/init.js
@@ -0,0 +1,43 @@
+new WOW().init();
+            $(document).ready(function(){
+		        $('.slider').slider({full_width: true,indicators: false});
+		    });
+        	$(".button-collapse").sideNav();
+			   $(document).ready(function(){
+				    $(window).scroll(function(){
+				    	console.log($(window).scrollTop()+" scroll height ");
+				        if($(window).scrollTop() > 550){
+				        	
+				            $("nav").removeClass("transparent").addClass("teal").addClass("darken-2").addClass('customNav').addClass('z-depth-2').removeClass('z-depth-0');
+				        }else {
+			        $("nav").removeClass("teal").removeClass("darken-2").addClass("transparent").removeClass('z-depth-2').addClass('z-depth-0');
+			       }
+
+				    });
+				});
+			   $(document).ready(function(){
+			    $('.scrollspy').scrollSpy();
+			  });
+			    $(document).ready(function(){
+    				$('.modal-trigger').leanModal();
+  				});
+  				  var $form = $('#registration-form');
+  				$form.on('submit', function() { 
+  					 $('.submit').on('click',function() {
+    					$(this).prop("disabled",true);
+					});           
+  					$.post($form.attr('action'), $form.serialize())
+  					.done(function(data) {
+  						$('.submit').on('click',function() {
+    					$(this).prop("disabled",false);
+					});
+   					$(".message").html("<br><br>You've successfully registered !<br><br>");
+  				})
+  			.fail(function(jqXHR, textStatus, errorThrown) {
+  				$('.submit').on('click',function() {
+    					$(this).prop("disabled",false);
+					});
+    			$(".message").html("<br><br> Something went wrong, please try again.<br><br>");
+  			});
+  			return false;
+			}); 
